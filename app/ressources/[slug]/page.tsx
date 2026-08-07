310b74efa894a5321c7e51c35dd9adca4159265a
Nexora <nexora.solutions.fr@gmail.com>
2026-08-07 10:24:29 +0200

    Change ArticlePage to handle async params

--- app/ressources/[slug]/page.tsx
+++ app/ressources/[slug]/page.tsx
@@ -115,8 +115,9 @@ export function generateStaticParams() {
   return articles.map((a) => ({ slug: a.slug }))
 }
 
-export default function ArticlePage({ params }: { params: { slug: string } }) {
-  const article = articles.find((a) => a.slug === params.slug)
+export default async function ArticlePage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  const article = articles.find((a) => a.slug === slug)
   if (!article) notFound()
 
   return (
