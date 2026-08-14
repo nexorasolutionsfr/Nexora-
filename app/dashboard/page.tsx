de0cc40836b87d72634d82b561b6d90f25660547
Nexora <nexora.solutions.fr@gmail.com>
2026-08-14 02:58:29 +0200

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function DashboardPage() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold">
+        Nexora Dashboard
+      </h1>
+
+      <p className="mt-4">
+        Espace garage sécurisé
+      </p>
+    </main>
+  );
+}
